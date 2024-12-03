9860dd744917eafbc601799b8553322510217413
Jaime Lopez Diaz <jlopedia@myuax.com>
2024-12-03 20:27:47 +0100

    UML-Arquitectura

---
+++ app/src/main/java/com/example/ejercicio_1/PantallaPrincipalActivity.java
@@ -0,0 +1,28 @@
+package com.example.ejercicio_1;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.ejercicio_1.ui.activities.LoginActivity;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+public class PantallaPrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+}
